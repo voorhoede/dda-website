dc594ac790d108857c7d7d88f76ac01443697c9f
Jamerrone <james.p.perrone@protonmail.com>
2024-11-12 15:35:34 +0100

    Update date formatting to use locale from i18n module

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,5 +1,7 @@
+import { getLocale } from './i18n';
+
 export const formatDate = (date: string | Date) =>
-  new Date(date).toLocaleDateString('nl-NL', {
+  new Date(date).toLocaleDateString(getLocale(), {
     day: 'numeric',
     month: 'short',
     year: 'numeric',
