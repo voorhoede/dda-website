e9dcd83ad589cc5fb20ae3bcccf29fe00a1272a8
James Peter Perrone Jefferies <james.p.perrone@protonmail.com>
2024-11-15 16:47:39 +0100

    PartnerBanner component. (#19)
    * Add PartnerBanner component with styles and GraphQL fragment
    
    * Update PartnerBanner to use PartnerLogo and adjust data fetching
    
    * Add migration script to create Partner model with fields for Name and Logo
    
    * Localize PartnerBanner text for partner call-to-action

---
+++ src/blocks/PartnerBanner/PartnerBanner.tsx
@@ -0,0 +1,36 @@
+import { Button } from '@components/Button';
+import { Heading } from '@components/Heading';
+import { ImageParade, ImageParadeItem } from '@components/ImageParade';
+import type { PartnerLogoFragment } from '@lib/types/datocms';
+import { SRCImage } from 'react-datocms';
+import './PartnerBanner.css';
+import { t } from '@lib/i18n';
+
+export const PartnerBanner = ({
+  partners,
+}: {
+  partners: PartnerLogoFragment[];
+}) => {
+  return (
+    <section>
+      <div className="partner-banner__content">
+        <Heading displayLevel={4} level={2}>
+          {t('become_a_partner_cta_body')}
+        </Heading>
+        <Button icon="arrow-right">{t('become_a_partner_cta_label')}</Button>
+      </div>
+      {partners.length >= 8 && (
+        <ImageParade direction="right">
+          {partners.map(
+            (partner) =>
+              partner.logo.responsiveImage && (
+                <ImageParadeItem key={partner.logo.id}>
+                  <SRCImage data={partner.logo.responsiveImage} />
+                </ImageParadeItem>
+              ),
+          )}
+        </ImageParade>
+      )}
+    </section>
+  );
+};
---
+++ src/blocks/PartnerBanner/index.ts
@@ -0,0 +1,1 @@
+export { PartnerBanner } from './PartnerBanner';
