14606ca4a1b1f0a2d621be2f822f341860a52402
Jamerrone <james.p.perrone@protonmail.com>
2024-11-15 16:20:25 +0100

    Update PartnerBanner to use PartnerLogo and adjust data fetching

--- src/blocks/PartnerBanner/PartnerBanner.tsx
+++ src/blocks/PartnerBanner/PartnerBanner.tsx
@@ -1,14 +1,14 @@
 import { Button } from '@components/Button';
 import { Heading } from '@components/Heading';
 import { ImageParade, ImageParadeItem } from '@components/ImageParade';
-import type { MemberLogoFragment } from '@lib/types/datocms';
+import type { PartnerLogoFragment } from '@lib/types/datocms';
 import { SRCImage } from 'react-datocms';
 import './PartnerBanner.css';
 
 export const PartnerBanner = ({
   partners,
 }: {
-  partners: MemberLogoFragment[];
+  partners: PartnerLogoFragment[];
 }) => {
   return (
     <section>
@@ -18,16 +18,18 @@ export const PartnerBanner = ({
         </Heading>
         <Button icon="arrow-right">Word partner</Button>
       </div>
-      <ImageParade direction="right">
-        {partners.map(
-          (member) =>
-            member.logo?.responsiveImage && (
-              <ImageParadeItem>
-                <SRCImage data={member.logo?.responsiveImage} />
-              </ImageParadeItem>
-            ),
-        )}
-      </ImageParade>
+      {partners.length >= 8 && (
+        <ImageParade direction="right">
+          {partners.map(
+            (partner) =>
+              partner.logo.responsiveImage && (
+                <ImageParadeItem key={partner.logo.id}>
+                  <SRCImage data={partner.logo.responsiveImage} />
+                </ImageParadeItem>
+              ),
+          )}
+        </ImageParade>
+      )}
     </section>
   );
 };
