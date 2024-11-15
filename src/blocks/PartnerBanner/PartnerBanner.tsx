c2fcf1abfa192fcea64bda2603762c236119eb7c
Jamerrone <james.p.perrone@protonmail.com>
2024-11-15 13:32:35 +0100

    Add PartnerBanner component with styles and GraphQL fragment

---
+++ src/blocks/PartnerBanner/PartnerBanner.tsx
@@ -0,0 +1,33 @@
+import { Button } from '@components/Button';
+import { Heading } from '@components/Heading';
+import { ImageParade, ImageParadeItem } from '@components/ImageParade';
+import type { MemberLogoFragment } from '@lib/types/datocms';
+import { SRCImage } from 'react-datocms';
+import './PartnerBanner.css';
+
+export const PartnerBanner = ({
+  partners,
+}: {
+  partners: MemberLogoFragment[];
+}) => {
+  return (
+    <section>
+      <div className="partner-banner__content">
+        <Heading displayLevel={4} level={2}>
+          Wij bedanken onze trouwe partners
+        </Heading>
+        <Button icon="chevron-right">Word partner</Button>
+      </div>
+      <ImageParade direction="right">
+        {partners.map(
+          (member) =>
+            member.logo?.responsiveImage && (
+              <ImageParadeItem>
+                <SRCImage data={member.logo?.responsiveImage} />
+              </ImageParadeItem>
+            ),
+        )}
+      </ImageParade>
+    </section>
+  );
+};
