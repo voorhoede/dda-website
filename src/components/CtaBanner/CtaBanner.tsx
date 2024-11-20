0545c8d40668cd8d12460aa2f480a6c08f5ab7ac
sjoerdbeentjes <11621275+sjoerdbeentjes@users.noreply.github.com>
2024-11-20 16:41:26 +0100

    create CtaBanner component

---
+++ src/components/CtaBanner/CtaBanner.tsx
@@ -0,0 +1,107 @@
+import type { PropsWithChildren } from 'react';
+import { Column, Grid, type SpanOptions } from '@components/Grid';
+import { Text } from '@components/Text';
+import type { MemberLogoFragment } from '@lib/types/datocms';
+import { SRCImage } from 'react-datocms';
+import clsx from 'clsx';
+import './CtaBanner.css';
+import type { ResponsiveSpan } from '@components/Grid/Column';
+import { MemberLogo } from '@blocks/MemberLogo';
+
+interface CtaBannerProps {
+  members: MemberLogoFragment[];
+  message: string;
+}
+
+const logoSpanConfig = {
+  mobile: 4,
+  desktop: 3,
+} as ResponsiveSpan;
+
+export const CtaBanner = ({
+  members,
+  children,
+  message,
+}: PropsWithChildren<CtaBannerProps>) => {
+  const start = members.slice(0, 4);
+  const middle = members.slice(4, 6);
+  const end = members.slice(6);
+
+  return (
+    <Grid as="section" border>
+      <Column
+        span={{
+          mobile: 12,
+          desktop: 6,
+        }}
+      >
+        <Grid border className="cta-banner__logos">
+          {start.map(
+            (member, index) =>
+              member.logo?.responsiveImage && (
+                <Column
+                  key={member.logo.id}
+                  span={logoSpanConfig}
+                  className={clsx({
+                    'cta-banner__logo--large-screen-only': index === 3,
+                  })}
+                >
+                  <MemberLogo {...member} />
+                </Column>
+              ),
+          )}
+
+          <Column
+            key={middle[0]?.logo?.id}
+            span={logoSpanConfig}
+            className="cta-banner__logo--large-screen-only"
+          >
+            {middle[0]?.logo?.responsiveImage && <MemberLogo {...middle[0]} />}
+          </Column>
+
+          <Column
+            span={{
+              mobile: 12,
+              desktop: 6,
+            }}
+            className="cta-banner__message"
+          >
+            <Text className="cta-banner__message-text">{message}</Text>
+          </Column>
+
+          <Column
+            key={middle[1]?.logo?.id}
+            span={logoSpanConfig}
+            className="cta-banner__logo--large-screen-only"
+          >
+            {middle[1]?.logo?.responsiveImage && <MemberLogo {...middle[1]} />}
+          </Column>
+
+          {end.map(
+            (member, index) =>
+              member.logo?.responsiveImage && (
+                <Column
+                  key={member.logo.id}
+                  span={logoSpanConfig}
+                  className={clsx({
+                    'cta-banner__logo--large-screen-only': index === 3,
+                  })}
+                >
+                  <MemberLogo {...member} />
+                </Column>
+              ),
+          )}
+        </Grid>
+      </Column>
+      <Column
+        span={{
+          mobile: 12,
+          desktop: 6,
+        }}
+        className="cta-banner__content"
+      >
+        {children}
+      </Column>
+    </Grid>
+  );
+};
--- src/components/Grid/Column.tsx
+++ src/components/Grid/Column.tsx
@@ -10,7 +10,7 @@ import './Column.css';
 
 export type SpanOptions = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 
-type ResponsiveSpan = {
+export type ResponsiveSpan = {
   mobile?: SpanOptions;
   tablet?: SpanOptions;
   desktop?: SpanOptions;
