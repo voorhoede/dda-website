a642995ace07e8e6596d0ece6124c9acab00ee0f
sjoerdbeentjes <11621275+sjoerdbeentjes@users.noreply.github.com>
2024-11-27 15:35:44 +0100

    Use content collections for random entries

--- src/blocks/VacancyList/VacancyList.tsx
+++ src/blocks/VacancyList/VacancyList.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@components/Button";
-import { Heading } from "@components/Heading";
-import { Text } from "@components/Text";
-import type { VacancyListItemFragment } from "@lib/types/datocms";
-import "./VacancyList.css";
-import { t } from "@lib/i18n";
+import { Button } from '@components/Button';
+import { Heading } from '@components/Heading';
+import { Text } from '@components/Text';
+import type { VacancyListItemFragment } from '@lib/types/datocms';
+import './VacancyList.css';
+import { t } from '@lib/i18n';
 
 export const VacancyList = ({
   vacancies,
@@ -13,56 +13,52 @@ export const VacancyList = ({
   return (
     <ul className="vacancy-list">
       {vacancies.map((vacancy) => (
-        <>
-          {vacancy.company[0] && (
-            <li className="vacancy-list__item" key={vacancy.id}>
-              <Heading displayLevel={4} level="span">
-                {vacancy.title}
-              </Heading>
+        <li className="vacancy-list__item" key={vacancy.id}>
+          <Heading displayLevel={4} level="span">
+            {vacancy.title}
+          </Heading>
 
-              <dl className="vacancy-list__meta">
-                <dt className="a11y-sr-only">{t("work_language")}</dt>
-                <dd>
-                  <Text as="span" variant="subtext">
-                    {vacancy.language}
-                  </Text>
-                </dd>
-                <dt className="a11y-sr-only">{t("company")}</dt>
-                <dd>
-                  <Text as="span" variant="subtext">
-                    {vacancy.company[0].name}
-                  </Text>
-                </dd>
-                <dt className="a11y-sr-only">{t("contract_hours")}</dt>
-                <dd>
-                  <Text as="span" variant="subtext">
-                    {vacancy.weeklyHours}
-                  </Text>
-                </dd>
-                <dt className="a11y-sr-only">{t("location")}</dt>
-                <dd>
-                  <Text as="span" variant="subtext">
-                    {vacancy.company[0].location}
-                  </Text>
-                </dd>
-              </dl>
+          <dl className="vacancy-list__meta">
+            <dt className="a11y-sr-only">{t('work_language')}</dt>
+            <dd>
+              <Text as="span" variant="subtext">
+                {vacancy.language}
+              </Text>
+            </dd>
+            <dt className="a11y-sr-only">{t('company')}</dt>
+            <dd>
+              <Text as="span" variant="subtext">
+                {vacancy.company[0]?.name}
+              </Text>
+            </dd>
+            <dt className="a11y-sr-only">{t('contract_hours')}</dt>
+            <dd>
+              <Text as="span" variant="subtext">
+                {vacancy.weeklyHours}
+              </Text>
+            </dd>
+            <dt className="a11y-sr-only">{t('location')}</dt>
+            <dd>
+              <Text as="span" variant="subtext">
+                {vacancy.company[0]?.location}
+              </Text>
+            </dd>
+          </dl>
 
-              <div>
-                <Button
-                  as="a"
-                  height="narrow"
-                  href={vacancy.url}
-                  icon="external"
-                  level="secondary"
-                  target="_blank"
-                  variant="large"
-                >
-                  {t("see")}
-                </Button>
-              </div>
-            </li>
-          )}
-        </>
+          <div>
+            <Button
+              as="a"
+              height="narrow"
+              href={vacancy.url}
+              icon="external"
+              level="secondary"
+              target="_blank"
+              variant="large"
+            >
+              {t('see')}
+            </Button>
+          </div>
+        </li>
       ))}
     </ul>
   );
