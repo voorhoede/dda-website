574920cc818afc7d097856c794a71df25ba6458b
Selwyn <talk@selwyn.cc>
2024-12-09 10:21:35 +0100

    Clarify what each test assertion does with a message

--- main_test.ts
+++ main_test.ts
@@ -5,29 +5,38 @@ Deno.test("parsePipeSeparatedValue behaves as expected", () => {
   assertEquals(
     parsePipeSeparatedValue("https://example.com"),
     ["https://example.com"],
+    "no separator",
   );
 
   assertEquals(
     parsePipeSeparatedValue("https://example.nl|https://example.nl"),
     ["https://example.nl", "https://example.nl"],
+    "separator between",
   );
 
   assertEquals(
     parsePipeSeparatedValue("HQ|"),
     ["HQ"],
+    "ends with separator",
   );
 
   assertEquals(
-    parsePipeSeparatedValue(
-      "One | Two|And Three |",
-    ),
-    ["One", "Two", "And Three"],
+    parsePipeSeparatedValue("|Afdeling"),
+    ["Afdeling"],
+    "starts with separator",
   );
 
-  assertEquals(parsePipeSeparatedValue("|06123||||034123|"), [
-    "06123",
-    "034123",
-  ]);
+  assertEquals(
+    parsePipeSeparatedValue("One | Two|And Three |"),
+    ["One", "Two", "And Three"],
+    "mixed spacing",
+  );
+
+  assertEquals(
+    parsePipeSeparatedValue("|06123||||034123|"),
+    ["06123", "034123"],
+    "multiple separators with no value",
+  );
 });
 
 Deno.test("getUrlPathFilename", () => {
