85da1e0c4d602df41f2ff2911fd9cc27a7465ff6
Sjoerd Beentjes <11621275+sjoerdbeentjes@users.noreply.github.com>
2024-12-10 12:57:00 +0100

    Data fetching improvements (#38)
    * adjust data fetching method
    
    * Refactor EventList, PublicationList, and VacancyList components to utilize getDehydratedState for initial state management.

--- src/lib/react-query.tsx
+++ src/lib/react-query.tsx
@@ -1,24 +1,58 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  dehydrate,
+  HydrationBoundary,
+  keepPreviousData,
+  QueryClient,
+  QueryClientProvider,
+  type DehydratedState,
+} from '@tanstack/react-query';
 import { type ComponentType } from 'react';
 
-// Create a new QueryClient instance
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-});
+export type QueryClientProviderProps = {
+  initialState: DehydratedState;
+  initialParams: Record<string, string>;
+  initialUrl: string;
+};
+
+export type QueryClientProviderComponentProps = Omit<
+  QueryClientProviderProps,
+  'initialState'
+>;
 
 export function withQueryClientProvider<T extends object>(
   WrappedComponent: ComponentType<T>,
 ) {
+  // Create a new QueryClient instance
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        placeholderData: keepPreviousData,
+      },
+    },
+  });
+
   // Return a new component that includes the QueryClientProvider
-  return function WithQueryClientProviderComponent(props: T) {
+  return function WithQueryClientProviderComponent(
+    props: T & QueryClientProviderProps,
+  ) {
     return (
       <QueryClientProvider client={queryClient}>
-        <WrappedComponent {...props} />
+        <HydrationBoundary state={props.initialState}>
+          <WrappedComponent {...props} />
+        </HydrationBoundary>
       </QueryClientProvider>
     );
   };
+}
+
+export function getDehydratedState<T extends object>(
+  key: [string, Record<string, string>],
+  data: T,
+) {
+  const queryClient = new QueryClient();
+
+  queryClient.setQueryData(key, data);
+
+  return dehydrate(queryClient);
 }
