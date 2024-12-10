eedeca7d283afe989c1acd473c0cbfd92b01ae8e
sjoerdbeentjes <11621275+sjoerdbeentjes@users.noreply.github.com>
2024-12-10 12:08:54 +0100

    Update MemberLogo component to use 'fill' for responsive image cropping and refactor background color logic.

--- src/blocks/MemberLogo/MemberLogo.tsx
+++ src/blocks/MemberLogo/MemberLogo.tsx
@@ -1,13 +1,26 @@
 import type { MemberLogoFragment } from '@lib/types/datocms';
 import { SRCImage } from 'react-datocms';
+import { useMemo } from 'react';
 import './MemberLogo.css';
 
 export const MemberLogo = (props: MemberLogoFragment) => {
+  const backgroundColor = useMemo(() => {
+    if (props.brandColor?.hex) {
+      return props.brandColor.hex;
+    }
+
+    if (props.logo.responsiveImage?.bgColor === '#ffffff') {
+      return 'var(--color-black)';
+    }
+
+    return 'var(--color-white)';
+  }, [props.brandColor?.hex]);
+
   return (
     <div
       className="member-logo"
       style={{
-        backgroundColor: props.brandColor?.hex,
+        backgroundColor,
       }}
     >
       {props.logo?.responsiveImage && (
