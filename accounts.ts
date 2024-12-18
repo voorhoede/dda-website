d64aa60b61338147e4df25622f65a165c99425fa
Selwyn <talk@selwyn.cc>
2024-12-18 16:12:32 +0100

    Setup import for user invitations

--- accounts.ts
+++ accounts.ts
@@ -1,25 +1,29 @@
-import { parse as parseCsv } from "@std/csv/parse";
-import { buildClient, LogLevel } from "npm:@datocms/cma-client-node";
+import { parse as parseCsv } from '@std/csv/parse';
+import { buildClient, LogLevel } from 'npm:@datocms/cma-client-node';
 
 const client = buildClient({
-  apiToken: Deno.env.get("DATOCMS_API_TOKEN")!,
-  environment: "members-migration",
+  apiToken: Deno.env.get('DATOCMS_API_TOKEN')!,
+  environment: 'main',
   logLevel: LogLevel.BASIC,
 });
 
 if (import.meta.main) {
-  const accountsCsv = await Deno.readTextFile("./accounts.csv", {});
+  const accountsCsv = await Deno.readTextFile('./accounts.csv', {});
   const accountsData = parseCsv(
     accountsCsv,
     { skipFirstRow: true },
   );
+
+  for (const account of accountsData) {
+    await inviteEditor(account.Email);
+  }
 }
 
 function inviteEditor(email: string) {
-  const EDITOR_ROLE_ID = "301184";
+  const MEMBER_ROLE_ID = '301184';
 
   return client.siteInvitations.create({
     email,
-    role: { type: "role", id: EDITOR_ROLE_ID },
+    role: { type: 'role', id: MEMBER_ROLE_ID },
   });
 }
