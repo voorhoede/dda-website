af8986de0fdf47ab8349f613746c5f7d0cbaa3a5
sjoerdbeentjes <11621275+sjoerdbeentjes@users.noreply.github.com>
2025-01-06 10:58:16 +0100

    Only show events in the future in highlighted events

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,5 +1,14 @@
 import { getLocale } from './i18n';
 
+export const getToday = () => {
+  const year = new Date().getFullYear();
+  const month = String(new Date().getMonth() + 1).padStart(2, '0');
+  const day = String(new Date().getDate()).padStart(2, '0');
+  const today = `${year}-${month}-${day}`;
+
+  return today;
+};
+
 export const formatDate = (date: string | Date) =>
   new Date(date).toLocaleDateString(getLocale(), {
     day: 'numeric',
