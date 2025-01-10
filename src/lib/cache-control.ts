999aae01ae8c724267fc204dd5a1eea0d6f640e0
Sjoerd Beentjes <11621275+sjoerdbeentjes@users.noreply.github.com>
2025-01-10 13:05:43 +0100

    Enhance cache control settings in response headers with SWR (#83)

--- src/lib/cache-control.ts
+++ src/lib/cache-control.ts
@@ -1,18 +1,23 @@
 // 1 minute
 const defaultMaxAge = 0;
-
 // 1 hour
-const defaultCdnMaxAge = 60;
+const defaultCdnMaxAge = 3600;
+// 1 minute stale-while-revalidate
+const defaultStaleWhileRevalidate = 60;
 
 export const setCacheControl = (
   response: ResponseInit,
   maxAge: number = defaultMaxAge,
   cdnMaxAge: number = defaultCdnMaxAge,
+  staleWhileRevalidate: number = defaultStaleWhileRevalidate,
 ) => {
   const headers = response.headers as Headers;
 
   if (headers) {
     headers.set('Cache-Control', `public, max-age=${maxAge}`);
-    headers.set('CDN-Cache-Control', `public, max-age=${cdnMaxAge}`);
+    headers.set(
+      'CDN-Cache-Control',
+      `public, max-age=${cdnMaxAge}, stale-while-revalidate=${staleWhileRevalidate}`,
+    );
   }
 };
