4f0a3b1d6dcd4c48f9c10f3e7ddbbd4cae99db15
Luuk <luuk@voorhoede.nl>
2025-02-07 13:12:36 +0100

    Add publications list template

--- src/components/NewDataList/PublicationsList/PublicationsList.tsx
+++ src/components/NewDataList/PublicationsList/PublicationsList.tsx
@@ -1,12 +1,56 @@
-import { Column, Grid } from '@components/Grid';
-import './PublicationsList.css';
+import { t } from '@lib/i18n';
+import { formatDate } from '@lib/date';
+import clsx from 'clsx';
+import { DataList, DataListItem, DataListItemFooter } from '@components/DataList';
+import { TagList, TagListItem } from '@components/Tag';
+import { Text } from '@components/Text';
+import { Heading } from '@components/Heading';
+import { Button } from '@components/Button';
 
-export const PublicationsList = ({ data }) => { 
+import './PublicationsList.css';
+import type { PublicatiesQueryQuery } from '@lib/types/datocms';
+
+type Props = {
+  data: PublicatiesQueryQuery['items'];
+};
+
+export const PublicationsList = ({ data }: Props) => { 
   return (
-    <Grid as="ul">
-      {data.map((item) => (
-        <Column key={item.id} as="li" span={3} className='publications-list-item'>{ item.title }</Column>
+    <DataList className={clsx({ 'container-padding-x container-padding-y': data.length > 0 }) }>
+      {data?.map((publication) => (
+        <DataListItem key={publication.id}>
+          <TagList>
+            {publication.tags.map(({ name }) => (
+              <TagListItem key={name}>{name}</TagListItem>
+            ))}
+          </TagList>
+          <Heading displayLevel={4} level={3}>
+            {publication.title}
+          </Heading>
+          <DataListItemFooter>
+            <Text variant="subtext">
+              <time dateTime={publication._createdAt}>
+                {formatDate(publication._createdAt)}
+              </time>
+            </Text>
+            <Button
+              as="a"
+              height="narrow"
+              icon="arrow-right"
+              level="secondary"
+              variant="large"
+              href={`/publicaties/${publication.slug}/`}
+              targetArea="parent"
+            >
+              {t('read_more')}
+              <span className="a11y-sr-only">
+                {t('_about_subject', { subject: publication.title })}
+              </span>
+            </Button>
+          </DataListItemFooter>
+        </DataListItem>
       ))}
-    </Grid>
+      
+    </DataList>
   );
 };
