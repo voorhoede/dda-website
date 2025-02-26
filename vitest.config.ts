1e4f5f35e78a88af984bc33b07cd4b178d501590
Luuk <luuk@voorhoede.nl>
2025-02-26 14:51:15 +0100

    Fix lint error

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,9 @@
 import { getViteConfig } from 'astro/config';
 
 export default getViteConfig({
-  test: {
-    // Vitest configuration options
-    teardownTimeout: 1000,
-    reporters: process.env.GITHUB_ACTIONS ? ['dot', 'github-actions'] : ['dot'],
-  },
+  // test: {
+  //   // Vitest configuration options
+  //   teardownTimeout: 1000,
+  //   reporters: process.env.GITHUB_ACTIONS ? ['dot', 'github-actions'] : ['dot'],
+  // },
 });
