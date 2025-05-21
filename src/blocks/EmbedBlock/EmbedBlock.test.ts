fbc9fe5f51cccb84623c73cd1dbf40967b609c26
sjoerdbeentjes <11621275+sjoerdbeentjes@users.noreply.github.com>
2025-05-21 11:39:41 +0200

    Update EmbedBlock to support additional providers and improve rendering logic
    - Change datocmsEnvironment to 'embed-test'.
    - Enhance EmbedBlock to correctly render Twitter, YouTube, and Vimeo embeds with appropriate tests.
    - Refactor EmbedBlock.astro for improved readability and structure.
    - Update TextBlock fragment to include EmbedBlock support.

--- src/blocks/EmbedBlock/EmbedBlock.test.ts
+++ src/blocks/EmbedBlock/EmbedBlock.test.ts
@@ -3,6 +3,33 @@ import { describe, expect, test } from 'vitest';
 import EmbedBlock, { type Props } from './EmbedBlock.astro';
 
 describe('EmbedBlock', () => {
+  test('renders a Twitter embed block correctly', async () => {
+    const fragment = await renderToFragment<Props>(EmbedBlock, {
+      props: {
+        block: {
+          id: '123',
+          url: 'https://twitter.com/devoorhoede/status/1670853955285565440',
+          data: {
+            provider_name: 'Twitter',
+            url: 'https://twitter.com/devoorhoede/status/1670853955285565440',
+            author_name: 'De Voorhoede',
+            author_url: 'https://twitter.com/devoorhoede',
+            html: '<blockquote class="twitter-tweet"><p lang="en" dir="ltr">💡🤔 What if you could build a universal Design System with <a href="https://twitter.com/reactjs?ref_src=twsrc%5Etfw">@reactjs</a> and use it in any web application or framework? <br><br>👨‍💻 We achieved this by compiling React to Web Blocks. <br><br>✨ This is how: <a href="https://t.co/pxHTsj2IE8">https://t.co/pxHTsj2IE8</a> <a href="https://t.co/ZfzQtniidF">pic.twitter.com/ZfzQtniidF</a></p>&mdash; De Voorhoede (@devoorhoede) <a href="https://twitter.com/devoorhoede/status/1670853955285565440?ref_src=twsrc%5Etfw">June 19, 2023</a></blockquote>\n<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>\n\n',
+            width: 550,
+            height: null,
+            type: 'rich',
+            cache_age: '3153600000',
+            provider_url: 'https://twitter.com',
+            version: '1.0',
+          },
+        },
+      },
+    });
+
+    expect(fragment.querySelector('default-embed')).toBeTruthy();
+    expect(fragment.querySelector('[data-provider="Twitter"]')).toBeTruthy();
+  });
+
   test('renders a Flickr embed block correctly', async () => {
     const fragment = await renderToFragment<Props>(EmbedBlock, {
       props: {
@@ -68,5 +95,73 @@ describe('EmbedBlock', () => {
 
     expect(fragment.querySelector('default-embed')).toBeTruthy();
     expect(fragment.querySelector('[data-provider="CodePen"]')).toBeTruthy();
+  });
+
+  test('renders a YouTube embed block correctly', async () => {
+    const fragment = await renderToFragment<Props>(EmbedBlock, {
+      props: {
+        block: {
+          id: '123',
+          url: 'https://www.youtube.com/watch?v=feylP4p1-KU&ab_channel=GreenCaravan',
+          data: {
+            provider_name: 'YouTube',
+            url: 'https://www.youtube.com/watch?v=feylP4p1-KU&ab_channel=GreenCaravan',
+            title: 'Green Caravan pitch (NL)',
+            author_name: 'Green Caravan',
+            author_url: 'https://www.youtube.com/@greencaravan6688',
+            type: 'video',
+            height: 113,
+            width: 200,
+            version: '1.0',
+            provider_url: 'https://www.youtube.com/',
+            thumbnail_height: 360,
+            thumbnail_width: 480,
+            thumbnail_url: 'https://i.ytimg.com/vi/feylP4p1-KU/hqdefault.jpg',
+            html: '<iframe width="200" height="113" src="https://www.youtube.com/embed/feylP4p1-KU?feature=oembed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen title="Green Caravan pitch (NL)"></iframe>'
+          },
+        },
+      },
+    });
+
+    expect(fragment.querySelector('video-embed')).toBeTruthy();
+    expect(fragment.querySelector('[data-provider="youtube"]')).toBeTruthy();
+  });
+
+  test('renders a Vimeo embed block correctly', async () => {
+    const fragment = await renderToFragment<Props>(EmbedBlock, {
+      props: {
+        block: {
+          id: '123',
+          url: 'https://vimeo.com/641535920',
+          data: {
+            provider_name: 'Vimeo',
+            url: 'https://vimeo.com/641535920',
+            type: 'video',
+            version: '1.0',
+            provider_url: 'https://vimeo.com/',
+            title: 'Structured text with inline records in DatoCMS',
+            author_name: 'De Voorhoede',
+            author_url: 'https://vimeo.com/voorhoede',
+            is_plus: '0',
+            account_type: 'pro',
+            html: '<iframe src="https://player.vimeo.com/video/641535920?app_id=122963" width="372" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" title="Structured text with inline records in DatoCMS"></iframe>',
+            width: 372,
+            height: 360,
+            duration: 35,
+            description: 'Companion article: https://www.voorhoede.nl/en/blog/enriching-rich-text-with-inline-components-datocms-react/',
+            thumbnail_url: 'https://i.vimeocdn.com/video/1290514162-5999b7e95c88cad3fe8cfcff2a3e1bd3a6e6cf4fe322cb95a_295x166',
+            thumbnail_width: 295,
+            thumbnail_height: 286,
+            thumbnail_url_with_play_button: 'https://i.vimeocdn.com/filter/overlay?src0=https%3A%2F%2Fi.vimeocdn.com%2Fvideo%2F1290514162-5999b7e95c88cad3fe8cfcff2a3e1bd3a6e6cf4fe322cb95a_295x166&src1=http%3A%2F%2Ff.vimeocdn.com%2Fp%2Fimages%2Fcrawler_play.png',
+            upload_date: '2021-11-02 10:17:54',
+            video_id: 641535920,
+            uri: '/videos/641535920'
+          },
+        },
+      },
+    });
+
+    expect(fragment.querySelector('video-embed')).toBeTruthy();
+    expect(fragment.querySelector('[data-provider="vimeo"]')).toBeTruthy();
   });
 });
