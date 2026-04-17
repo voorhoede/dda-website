76500c376cd378c20aeee34193fb8905aca72737
Luuk <luuk@voorhoede.nl>
2026-04-17 17:06:47 +0200

    fix: remove debug log for TURNSTILE_SITE_KEY in TurnstileWidget

--- src/components/TurnstileWidget/TurnstileWidget.tsx
+++ src/components/TurnstileWidget/TurnstileWidget.tsx
@@ -2,7 +2,6 @@ import { Turnstile } from 'react-turnstile';
 import { TURNSTILE_SITE_KEY } from 'astro:env/client';
 
 export const TurnstileWidget = () => {
-  console.log('TURNSTILE_SITE_KEY', TURNSTILE_SITE_KEY);
   const isClient = typeof window !== 'undefined';
 
   if (!isClient) {
